3e4d85428ea16e65e5187529cb3380607fdfcb51
Vadim Kozelskij <vadim.kozelskij@mail.ru>
2024-10-03 20:52:13 +0300

    добавил класс пользовательского интерфейса программы

--- lesson4 solutions/main.rb
+++ lesson4 solutions/main.rb
@@ -1,47 +1,21 @@
-require_relative 'train.rb'
-require_relative 'cargo_train.rb'
-require_relative 'passenger_train.rb'
-require_relative 'wagon.rb'
-require_relative 'cargo_wagon.rb'
-require_relative 'passenger_wagon.rb'
-require_relative 'station.rb'
-require_relative 'route.rb'
+require_relative 'interface.rb'
 
+puts "Выберите действие:\n
+      1. Создать станцию\n
+      2. Создать поезд\n
+      3. Создать маршрут\n
+      4. Добавить станцию в маршруте\n
+      5. Удалить станцию с маршрута\n
+      6. Назначить маршрут поезду\n
+      7. Добавить вагон к поезду\n
+      8. Отцепить вагон от поезда\n
+      9. Переместить поезд по маршруту\n
+      10. Просмотреть список станций\n
+      11. Просмотреть список поездов на станции\n
+       0. Выход"
 
-# puts 'Предлагаем вашему внимаю консольный симулятор жд станции \
-#       Давайте начнём попорядку, создадим маршрут, для этого нужно создать две станции \
-#       начальную и конечную \ 
-#       Введите название начальной станции'
+user_input = gets.chomp.to_i
 
-# start_station_input = gets.chomp.to_s 
+interface = Interface.new 
 
-# start_station = Station.new(start_station_input)
-
-# puts 'Теперь ведите название конечной станции'
-
-# end_station_input = gets.chomp.to_s 
-
-# end_station = Station.new(end_station_input)
-
-# route = Route.new(start_station, end_station)
-
-# puts 'Отлично, две станции и маршрут созданы'
-
-# puts 'Теперь нужно создать поезд. Придумайте номер поезда'
-
-# number_train_input = gets.to_i
-
-# puts 'Укажите тип поезда: 0 - Пассажиский, 1 - Гузовой'
-
-# type_train_input = gets.to_i
-
-# train = if type_train_input == 0
-#   PassengerTrain.new(number_train_input)
-# elsif type_train_input == 0  
-#   CargoTrain.new(number_train_input)
-# else
-#   return puts 'Некорректное значение, введите 0 или 1'
-# end  
-
-# # Возможно спроектировать всё так, чтоб спрашивать ввод от пользователя в цикле
-# # to be contined...   
+interface.choose_user(user_input)
