c9ff73e40cbb24366ff2d91ad316f0f6d6f5fb22
Vadim Kozelskij <vadim.kozelskij@mail.ru>
2024-12-07 12:23:59 +0300

    BlackJack init

---
+++ BlackJack/diller.rb
@@ -0,0 +1,7 @@
+class Diller
+  attr_accessor :cash
+
+  def initialize
+    @cash = 0
+  end
+end
---
+++ BlackJack/game.rb
@@ -0,0 +1,20 @@
+class Game  
+
+  def initialize(player1, player2) 
+    @game_bank = 0
+    @cards = []
+  end
+
+  def start_game
+  end
+
+  def end_game 
+  end 
+  
+  def count_points
+  end
+
+  def interface
+  end
+
+end
